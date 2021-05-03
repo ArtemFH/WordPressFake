c1678ca050c0e1e70aa1f4fd66ca869a8de1b4e9
artem <artembereza1@mail.ru>
2021-05-04 00:50:19 +0700

    Laravel 19-20-21

---
+++ app/Models/Posts.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Models;
+
+use Carbon\Carbon;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\URL;
+
+class Posts extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'id',
+        'image',
+        'description'
+    ];
+
+    public function getImageAttribute()
+    {
+        return URL::to('/') . '/storage/' . $this->attributes['image'];
+    }
+
+    public function getCreatedAtAttribute($date)
+    {
+        return Carbon::createFromFormat('Y-m-d H:i:s', $date)->format('Y-m-d');
+    }
+//    public $timestamps = false;
+}
