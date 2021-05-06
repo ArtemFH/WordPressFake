2c4d986d48777ddc476fe7eef94d61d7dd3aedff
artem <artembereza1@mail.ru>
2021-05-06 07:09:48 +0700

    Laravel 19-20-21

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,7 @@ use Illuminate\Support\Facades\Route;
 Route::name('home.')->group(function () {
     Route::get('/', [\App\Http\Controllers\PostController::class, 'index'])->name('head');
 });
+
+//Route::name('home.')->group(function () {
+//    Route::get('/', [\App\Http\Controllers\PostController::class, 'index'])->name('head');
+//});
